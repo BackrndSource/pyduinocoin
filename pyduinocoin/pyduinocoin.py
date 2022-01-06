dafefd108b3602291c7eed9b3af53dc55d2d3f57
backrndsource <backrndsource@gmail.com>
2022-01-06 15:15:42 +0100

    Updating DuinoClient.
    Solve an error occurred when trying to get statistics.
    Now raise an exception if server response is not 200
    Other minor corrections

--- pyduinocoin/pyduinocoin.py
+++ pyduinocoin/pyduinocoin.py
@@ -17,81 +17,86 @@ class DuinoClient():
         '''
         url = '{}{}'.format(self._base_url, endpoint)
         response = requests.request("GET", url, params=params)
-        json = response.json()
 
-        if ('success' in json and json['success'] == False) or 'message' in json:
-            raise PyDuinoCoinException(json['message'] if 'message' in json else 'Unknown error')
+        if response.status_code == 200:
+            json = response.json()
 
-        if results_key:
-            if isinstance(json[results_key], list):
-                return [DictObj(item) if isinstance(item, dict) else item for item in json[results_key]]
-            else:
-                return DictObj(json[results_key])
+            if ('success' in json and json['success'] == False) or 'message' in json:
+                raise PyDuinoCoinException(json['message'] if 'message' in json else 'Unknown error')
 
-        return DictObj(json)
+            if results_key:
+                if isinstance(json[results_key], list):
+                    return [DictObj(item) if isinstance(item, dict) else item for item in json[results_key]]
+                else:
+                    return DictObj(json[results_key])
+
+            return DictObj(json)
+        else:
+            raise PyDuinoCoinException('SERVER REQUEST ERROR (' + str(response.status_code) + ')')
+
 
     def user(self, username):
         '''
         Return username's balance, last transactions and miners in one request.
         '''
 
-        return self._get('users/'+username)
+        return self._get('users/'+str(username))
 
     def auth(self, username, password):
         '''
         Check username's password.
         '''
         
-        return self._get('auth/'+username, {'password':password})
+        return self._get('auth/'+str(username), {'password':password})
 
     def transactions(self, hash=None):
         '''
         Return all transactions or an unique transaction with that hash.
         '''
         
-        return self._get('transactions/'+hash) if hash else self._get('transactions') 
+        return self._get('transactions/'+str(hash)) if hash else self._get('transactions') 
 
     def user_transactions(self, username):
         '''
         Return transactions related to username.
         '''
 
-        return self._get('user_transactions/'+username)
+        return self._get('user_transactions/'+str(username))
 
     def id_transactions(self, id):
         '''
         Return a transaction with that id.
         '''
 
-        return self._get('id_transactions/'+id)
+        return self._get('id_transactions/'+str(id))
 
     def transfer(self, username, password, recipient, amount, memo='Powered by pyduinocoin'):
         '''
         Transfer funds from username to recipient.
         '''
 
-        return self._get('transaction', {'username':username, 'password':password, 'recipient':recipient, 'amount':amount, 'memo':memo})
+        return self._get('transaction', {'username':username, 'password':password, 'recipient':recipient, 'amount':amount, 'memo':memo}, results_key=None)
 
     def miners(self, username=None):
         '''
         Return all miners or username's miners.
         '''
 
-        return self._get('miners/'+username) if username else self._get('miners')
+        return self._get('miners/'+str(username)) if username else self._get('miners')
     
     def balances(self, username=None):
         '''
         Return all balances or username's balance.
         '''
 
-        return self._get('balances/'+username) if username else self._get('balances')
+        return self._get('balances/'+str(username)) if username else self._get('balances')
 
     def statistics(self):
         '''
         Return server statistics.
         '''
 
-        return self._get('statistics')
+        return self._get('statistics', results_key=None)
 
     def all_pools(self):
         '''
@@ -105,4 +110,4 @@ class DuinoClient():
         Submit exchange request in the DUCO Exchange.
         '''
 
-        return self._get('transaction', {'username':username, 'password':password, 'email':email, 'type':type, 'amount':amount, 'coin':coin, 'address':address})
+        return self._get('transaction', {'username':username, 'password':password, 'email':email, 'type':type, 'amount':amount, 'coin':coin, 'address':address}, results_key=None)
--- tests/pyduinocoin_test.py
+++ tests/pyduinocoin_test.py
@@ -1,22 +1,105 @@
 from unittest import TestCase
 from pyduinocoin import DuinoClient, DictObj
 
+
 class DuinoClientTests(TestCase):
     def setUp(self):
         self.client = DuinoClient()
 
     def test_get_user(self):
-        result = self.client.user('Backrndsource')
-        self.assertTrue(isinstance(result, DictObj))
-        
-        self.assertTrue(hasattr(result, 'balance'))
-        self.assertTrue(isinstance(result.balance, DictObj))
-        self.assertTrue(hasattr(result.balance, 'balance'))
-        self.assertTrue(hasattr(result.balance, 'username'))
-        self.assertEqual(result.balance.username, 'Backrndsource')
-        
-        self.assertTrue(hasattr(result, 'miners'))
-        self.assertTrue(isinstance(result.miners, list))
+        try:
+            result = self.client.user('Backrndsource')
+        except Exception as exception:
+            self.assertIn('SERVER REQUEST ERROR', exception.args[0])
+            print(exception)
+        else:
+            self.assertTrue(isinstance(result, DictObj))
+            
+            self.assertTrue(hasattr(result, 'balance'))
+            self.assertTrue(isinstance(result.balance, DictObj))
+            self.assertTrue(hasattr(result.balance, 'balance'))
+            self.assertTrue(hasattr(result.balance, 'username'))
+            self.assertEqual(result.balance.username, 'Backrndsource')
+            
+            self.assertTrue(hasattr(result, 'miners'))
+            self.assertTrue(isinstance(result.miners, list))
 
-        self.assertTrue(hasattr(result, 'transactions'))
-        self.assertTrue(isinstance(result.transactions, list))
+            self.assertTrue(hasattr(result, 'transactions'))
+            self.assertTrue(isinstance(result.transactions, list))
+
+            print(result)
+    
+    def test_get_user_transactions(self):
+        try:
+            user_transactions = self.client.user_transactions('Backrndsource')
+        except Exception as exception:
+            self.assertIn('SERVER REQUEST ERROR', exception.args[0])
+            print(exception)
+        else:
+            self.assertTrue(isinstance(user_transactions, list))
+
+            for transaction in user_transactions:  
+                self.assertTrue(isinstance(transaction, DictObj))
+
+            print(user_transactions)
+
+    def test_get_miners(self):
+        try:
+            miners = self.client.miners()
+        except Exception as exception:
+            self.assertIn('SERVER REQUEST ERROR', exception.args[0])
+            print(exception)
+        else:
+            self.assertTrue(isinstance(miners, DictObj))
+            print(miners)
+
+    def test_get_user_miners(self):
+        try:
+            miners = self.client.miners('Backrndsource')
+        except Exception as exception:
+            self.assertIn('SERVER REQUEST ERROR', exception.args[0])
+            print(exception)
+        else:
+            self.assertTrue(isinstance(miners, list))
+            print(miners)
+
+    def test_get_balances(self):
+        try:
+            balances = self.client.balances()
+        except Exception as exception:
+            self.assertIn('SERVER REQUEST ERROR', exception.args[0])
+            print(exception)
+        else:
+            self.assertTrue(isinstance(balances, DictObj))
+            print(balances)
+
+    def test_get_user_balances(self):
+        try:
+            balances = self.client.balances('Backrndsource')
+        except Exception as exception:
+            self.assertIn('SERVER REQUEST ERROR', exception.args[0])
+            print(exception)
+        else:
+            self.assertTrue(isinstance(balances, DictObj))
+            print(balances)
+
+    def test_get_statistics(self):
+        try:
+            statistics = self.client.statistics()
+        except Exception as exception:
+            self.assertIn('SERVER REQUEST ERROR', exception.args[0])
+            print(exception)
+        else:
+            self.assertTrue(isinstance(statistics, DictObj))
+            print(statistics)
+
+    def test_get_all_pools(self):
+        try:
+            all_pools = self.client.all_pools()
+        except Exception as exception:
+            self.assertIn('SERVER REQUEST ERROR', exception.args[0])
+            print(exception)
+        else:
+            self.assertTrue(isinstance(all_pools, list))
+            print(all_pools)
+            
